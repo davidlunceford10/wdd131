4dff4523977a94a80d60b34798261ab06619af37
David Lunceford <davidlunceford10@yahoo.com>
2025-01-28 05:50:28 -0700

    coolpix completed

---
+++ coolpics/coolpics.js
@@ -0,0 +1,6 @@
+// coolpics.js
+document.getElementById('menu').addEventListener('click', function () {
+    const links = document.getElementById('links');
+    links.style.display = (links.style.display === 'flex' ? 'none' : 'flex');
+  });
+  
